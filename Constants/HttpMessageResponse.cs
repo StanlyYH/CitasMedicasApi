1cec167f17c7ba4796d184763c2a3d4d7c15b27b
StanlyYH <stanlyyh@icloud.com>
2026-04-13 23:46:04 -0600

    Estructura Inicial del Proyecto

---
+++ Constants/HttpMessageResponse.cs
@@ -0,0 +1,12 @@
+namespace CitasMedicasApi.Constants
+{
+    public class HttpMessageResponse
+    {
+        public const string REGISTER_NOT_FOUND = "Record not found.";
+        public const string REGISTER_FOUND = "Record found.";
+        public const string REGISTER_CREATED = "Record created successfully.";
+        public const string REGISTER_UPDATED = "Record updated successfully.";
+        public const string REGISTER_DELETED = "Record deleted successfully.";
+        public const string REGISTERS_FOUND = "Records found.";
+    }
+}
